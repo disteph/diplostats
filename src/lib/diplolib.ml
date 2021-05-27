b7f3efce1efa05cf027494f100b0e05c760dbe22
Stephane Graham-Lengrand <disteph@gmail.com>
2021-05-26 19:45:44 -0700

    dune + server

---
+++ src/lib/diplolib.ml
@@ -0,0 +1,248 @@
+open Containers
+open Cohttp
+open Cohttp_lwt_unix
+
+let verbosity = ref 0
+let anonymity = ref true
+let no_messaging = ref true
+let norm_messaging = ref false
+let rule_messaging = ref false
+let pub_messaging = ref false
+let finished = ref false
+
+let vdiplo   = ref true
+let webdiplo = ref true
+
+let bogus_games = ["Caucasia - Random" ]
+
+let print i fs = Format.((if !verbosity >= i then fprintf else ifprintf) stderr) fs
+let print_stdout s = Format.(fprintf stdout "%s@," s)
+             
+type base = VDiplo | WebDiplo [@@deriving show]
+
+let url base variant page = 
+  let base = match base with
+    | VDiplo -> "https://vdiplomacy.com/gamelistings.php?"
+    | WebDiplo -> "https://webdiplomacy.net/gamelistings.php?"
+  in
+  let options = [
+      "gamelistType", Some "Search";
+      "status", Some (if !finished then "Finished" else "Won");
+      "userGames", Some "All";
+      "seeJoinable", Some "All";
+      "privacy", Some "All";
+      "potType", Some "All";
+      "drawVotes", Some "All";
+      "variant", Some variant;
+      "excusedTurns", Some "All";
+      "anonymity", if !anonymity then Some "yes" else None;
+      "phaseLengthMin", Some "All";
+      "rrMin", Some "All";
+      "rrMax", Some "All";
+      "betMin", Some "";
+      "betMax", Some "";
+      "messageNon", (if !no_messaging then Some "Yes" else None);
+      "messageNorm", (if !norm_messaging then Some "Yes" else None);
+      "messageRule", (if !rule_messaging then Some "Yes" else None);
+      "messagePub", (if !pub_messaging then Some "Yes" else None);
+      "sortCol", Some "id";
+      "sortType", Some "desc";
+      "Submit", Some "Search";
+      "pagenum", Some (string_of_int page)
+    ]
+  in
+  let rec aux1 = function
+    | [] -> []
+    | (o,Some v)::tail -> (o^"="^v)::aux1 tail
+    | (_o,None)::tail -> aux1 tail
+  in
+  let rec aux2 = function
+    | [] -> ""
+    | [o] -> o
+    | o::tail -> o^"&"^aux2 tail
+  in
+  base^(options |> aux1 |> aux2)
+               
+let get base variant page =
+  let url = url base variant page in
+  let open Lwt in
+  Client.get (Uri.of_string url) >>= fun (_resp, body) ->
+  (* let code = resp |> Response.status |> Code.code_of_status in
+   * Printf.printf "Response code: %d\n" code;
+   * Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string); *)
+  body |> Cohttp_lwt.Body.to_string >|= fun body ->
+  (* Printf.printf "Body of length: %d\n" (String.length body); *)
+  body
+
+let get_name country =
+  let open Soup in
+  country $? ".memberCountryName" |> Option.map R.leaf_text
+
+let get_centers_units country =
+  let open Soup in
+  match country $? ".memberSCCount" with
+  | None -> None
+  | Some block ->
+     let is_em node =
+       match element node with
+       | Some node -> String.equal_caseless (name node) "em"
+       | None -> false
+     in 
+     let below = descendants block |> to_list
+                 |> List.filter is_em
+                 |> List.map texts
+     in
+     match below with
+     | [[centers]; [units]] ->
+        (try
+           Some(int_of_string centers, int_of_string units)
+         with _ -> None)
+     | _ -> None
+
+type quant =
+  | Survived of int*int
+  | Defeated of int [@@deriving show { with_path = false }]
+
+type result = {
+    country : string;
+    centers_units: quant
+  } [@@deriving show { with_path = false }]
+
+type record = {
+    result : result;
+    ranking : float ref [@printer fun fmt r -> fprintf fmt "%f" !r];
+    victorious : float ref [@printer fun fmt r -> fprintf fmt "%f" !r];
+  } [@@deriving show { with_path = false }]
+
+let compare_result { centers_units = cu1; _ } { centers_units = cu2; _ } =
+  match cu1, cu2 with
+  (* | Survived(c1,u1), Survived(c2,u2) -> Ord.pair Int.compare Int.compare (c1,u1) (c2,u2) *)
+  | Survived(c1,_), Survived(c2,_) -> Int.compare c1 c2
+  | Survived _, Defeated _ -> 1
+  | Defeated _, Survived _ -> -1
+  | Defeated _d1, Defeated _d2 -> 0
+  (* | Defeated d1, Defeated d2 -> Ord.opp Int.compare d1 d2 *)
+
+module HT = Hashtbl.Make(String)
+
+let (/) n d = float_of_int n /. float_of_int d
+
+let game_name game =
+  let open Soup in
+  game $ ".gameName" |> R.leaf_text
+
+let compute_stats l =
+  let rec aux i rankings victories centers units = function
+    | [] -> rankings /. float_of_int i, victories /. float_of_int  i, centers / i, units / i
+    | { ranking; result = { centers_units = Survived(c,u); _ }; victorious }::l ->
+       let victories = victories +. !victorious in
+       aux (i+1) (rankings +. !ranking) victories (centers + c) (units + u) l
+    | { ranking; result = { centers_units = Defeated _r ; _ } ; _}::l ->
+       aux (i+1) (rankings +. !ranking) victories centers units l
+  in
+  aux 0 0. 0. 0 0 l
+
+let close_ranking last_ranking index =
+  let nb = float_of_int index -. !last_ranking in
+  let h = (nb -. 1.) /. 2. in
+  last_ranking := !last_ranking +. h
+
+  
+let compute_ranking last_record index result =
+  let index = index+1 in
+  match last_record with
+  | None -> let ranking = ref 1. in
+            let victorious = ref 1. in
+            let record = { ranking; victorious; result } in
+            (Some record), record
+  | Some r ->
+     if compare_result r.result result = 0
+     then last_record, { ranking = r.ranking; victorious = r.victorious; result }
+     else
+       begin
+         close_ranking r.ranking index;
+         r.victorious := !(r.victorious) /. float_of_int (index - 1);
+         let ranking = ref (float_of_int index) in
+         let victorious = ref 0. in
+         let record = { ranking; victorious; result } in
+         (Some record), record
+       end
+    
+let parse ?older variant =
+  Format.(fprintf stdout "@[<v>");
+  let open Soup in
+  let rec collect base page games =
+    print 0 "@[Collecting page %i from %a@]@,%!" page pp_base base;
+    let parsed = Lwt_main.run (get base variant page) |> parse in
+    match parsed $$ ".gamePanel" |> to_list with
+    | [] -> games
+    | page_games -> collect base (page+1) (List.rev_append page_games games)
+  in
+  print 0 "\n%!";
+  let webgames = if !webdiplo then collect WebDiplo 1 [] else [] in
+  let vgames   = if !vdiplo   then collect VDiplo 1 [] else [] in
+  let tbl = HT.create 10 in
+  let per_country (i,d,l) member =
+    match get_name member with
+    | Some country ->
+       let centers_units, d =
+         match get_centers_units member with
+         | Some(a,b) -> Survived(a,b), d
+         | None -> Defeated d, d+1
+       in
+       let record = { country; centers_units }
+       in
+       (* print_stdout (show_result record); *)
+       i+1, d, (record::l)
+    | None -> i, d, l
+  in
+  let per_game older ((stop,nb) as sofar) game =
+    if stop then sofar
+    else
+      let name = game_name game in
+      begin
+        print 0 "@[%s@]@,%!" name;
+        match older with
+        | Some g when String.equal name g -> true, nb
+        | _ ->
+           if List.mem name bogus_games then (false, nb)
+           else
+             let _, _, results = game $$ ".member" |> to_list |> List.fold_left per_country (1,1,[]) in
+             let results = List.sort (Ord.opp compare_result) results in
+             let last_record, results = List.fold_map_i compute_ranking None results in
+             (match last_record with
+              | Some r ->
+                 close_ranking r.ranking (List.length results + 1);
+                 r.victorious := !(r.victorious) /. float_of_int (List.length results)
+              | None -> assert false);
+             print 1 "@[<v>%a@,@]@,%!" (List.pp pp_record) results;
+             let fill_up record =
+               let result = record.result in
+               let base = if HT.mem tbl result.country then HT.find tbl result.country else [] in
+               HT.replace tbl result.country (record::base)
+             in
+             List.iter fill_up results;
+             false, nb+1
+      end
+  in
+  let sofar = List.fold_left (per_game older) (false,0) webgames in
+  let _, nb_games = List.fold_left (per_game older) sofar vgames in
+  let upto = match older with
+    | Some g -> " before game "^g
+    | None -> ""
+  in
+  print_stdout (variant^"; "^string_of_int nb_games ^"; games played"^ upto ^"; "^ url VDiplo variant 1);
+  print_stdout "Rémi; Country ; Average ranking; Victories; Average centers; Average units";
+  let total = ref 0. in
+  let aux country l =
+    let ranking, victories, centers, units = compute_stats l in
+    total := ranking +. !total;
+    print_stdout ("; "^ country
+                   ^"; "^ string_of_float ranking
+                   ^"; "^ string_of_float victories
+                   ^"; "^ string_of_float centers
+                   ^"; "^ string_of_float units)
+  in
+  HT.iter aux tbl;
+  print 1 "@[<v>%f@,@]@,%!" !total;
+  Format.sprintf "@]%!"
---
+++ src/server.ml
@@ -0,0 +1,176 @@
+open Containers
+open Sexplib
+   
+open Lwt.Infix
+open Cohttp_lwt
+open Cohttp_lwt_unix
+
+open Diplostats.Diplolib
+
+(* Apply the [Webmachine.Make] functor to the Async-based IO module exported by
+ * cohttp. For added convenience, include the [Rd] module as well so you don't
+ * have to go reaching into multiple modules to access request-related
+ * information. *)
+module Wm = struct
+  module Rd = Webmachine.Rd
+  module UnixClock = struct
+    let now = fun () -> int_of_float (Unix.gettimeofday ())
+  end
+  include Webmachine.Make(Cohttp_lwt_unix__Io)(UnixClock)
+end
+
+(* Create a new class that inherits from [Wm.resource] and provides
+ * implementations for its two virtual methods, and overrides some of its default methods.
+ *)
+class hello = object(self)
+  inherit [Body.t] Wm.resource
+
+  (* Only allow POST requests to this resource *)
+  method allowed_methods rd =
+    Wm.continue [`GET] rd
+
+  (* Setup the resource to handle multiple content-types. Webmachine will
+   * perform content negotiation as described in RFC 7231:
+   *
+   *   https://tools.ietf.org/html/rfc7231#section-5.3.2
+   *
+   * Content negotiation can be a complex process. Hoever for simple Accept
+   * headers its fairly straightforward. Here's what content negotiation will
+   * produce in some of these simple cases:
+   *
+   *     Accept             | Called method
+   *   ---------------------+----------------
+   *     "text/plain"       | self#to_text
+   *     "text/html"        | self#to_html
+   *     "text/*"           | self#to_html
+   *     "application/json" | self#to_json
+   *     "application/*"    | self#to_json
+   *     "*/*"              | self#to_html
+   *)
+  method content_types_provided rd =
+    Wm.continue [
+      ("text/html"       , self#to_html);
+      ("text/plain"      , self#to_text);
+      ("application/json", self#to_json);
+    ] rd
+
+  method content_types_accepted rd =
+    Wm.continue [] rd
+
+    
+  method private answer rd =
+    let treat key f =
+      let open Wm.Rd in
+      match lookup_path_info key rd with
+      | Some v -> f v
+      | None -> ()
+    in
+    match Wm.Rd.lookup_path_info "variant" rd with
+    | None -> "Variant is missing"
+    | Some variant ->
+       let () =
+         treat "verb"     (fun v -> verbosity := int_of_string v);
+         treat "finished" (fun v -> finished := bool_of_string v);
+         treat "nono_messaging" (fun v -> no_messaging := not(bool_of_string v));
+         treat "norm_messaging" (fun v -> norm_messaging := bool_of_string v);
+         treat "rule_messaging" (fun v -> rule_messaging := bool_of_string v);
+         treat "pub_messaging"  (fun v -> pub_messaging := bool_of_string v);
+         treat "no-anonymity"   (fun v -> anonymity := not(bool_of_string v));
+         treat "novdiplo"       (fun v -> vdiplo := not(bool_of_string v));
+         treat "nowebdiplo"     (fun v -> webdiplo := not(bool_of_string v))
+       in
+       let older = Wm.Rd.lookup_path_info "older" rd in
+       parse ?older variant
+                                                             
+  (* Returns an html-based representation of the resource *)
+  method private to_html rd =
+    let answer = self#answer rd in
+    let body = Printf.sprintf "<html><body>%s</body></html>" answer in
+    Wm.continue (`String body) rd
+
+  (* Returns a plaintext representation of the resource *)
+  method private to_text rd =
+    let text = self#answer rd in
+    Wm.continue (`String text) rd
+
+  (* Returns a json representation of the resource *)
+  method private to_json rd =
+    let answer = self#answer rd in
+    let json = Printf.sprintf "{\"output\" : \"%s\"}" answer in
+    Wm.continue (`String json) rd
+
+end
+
+let main ~port =
+  (* The route table. Both routes use the [hello] resource defined above.
+   * However, the second one containes the [:what] wildcard in the path. The
+   * value of that wildcard can be accessed in the resource by calling
+   *
+   *   [Wm.Rd.lookup_path_info "what" rd]
+  *)
+  let routes = [
+    ("/"           , fun () -> new hello);
+  ] in
+  let callback (_ch,_conn) request body =
+    let open Cohttp in
+    (* Perform route dispatch. If [None] is returned, then the URI path did not
+     * match any of the route patterns. In this case the server should return a
+     * 404 [`Not_found]. *)
+    Wm.dispatch' routes ~body ~request
+    >|= begin function
+      | None        -> (`Not_found, Header.init (), `String "Not found", [])
+      | Some result -> result
+    end
+    >>= fun (status, headers, body, path) ->
+    (* If you'd like to see the path that the request took through the
+     * decision diagram, then run this example with the [DEBUG_PATH]
+     * environment variable set. This should suffice:
+     *
+     *  [$ DEBUG_PATH= ./hello_async.native]
+     *
+    *)
+    let path =
+      match Sys.getenv "DEBUG_PATH" with
+      | _ -> Printf.sprintf " - %s" (String.concat ", " path)
+      | exception Not_found   -> ""
+    in
+    Printf.eprintf "%d - %s %s%s"
+      (Code.code_of_status status)
+      (Code.string_of_method (Request.meth request))
+      (Uri.path (Request.uri request))
+      path;
+    (* Finally, send the response to the client *)
+    Server.respond ~headers ~body ~status ()
+  in
+  (* Create the server and handle requests with the function defined above. Try
+   * it out with some of these curl commands:
+   *
+   *   [curl -H"Accept:text/html" "http://localhost:8080"]
+   *   [curl -H"Accept:text/plain" "http://localhost:8080"]
+   *   [curl -H"Accept:application/json" "http://localhost:8080"]
+  *)
+  let conn_closed (ch,_conn) =
+    Printf.printf "connection %s closed\n%!"
+      (Sexplib.Sexp.to_string_hum (Conduit_lwt_unix.sexp_of_flow ch))
+  in
+  let config = Server.make ~callback ~conn_closed () in
+  Printf.eprintf "hello_lwt: listening on localhost:%d\n%!" port;
+  Server.create  ~mode:(`TCP(`Port port)) config
+
+let port = ref 8080
+
+let description arg =
+  "This is an ocaml reformulator for "^arg^" represented in JSON. The command takes the port number to listen to as optional argument (8080 is the default)."
+
+let args = ref []
+let description = "Diplostats server"
+
+let options = [
+  ("-port", Arg.Int(fun i -> port := i), "port (default is 8080)");
+];;
+
+Arg.parse options (fun a->args := a::!args) description;;
+
+match !args with
+| [] -> Lwt_main.run (main ~port:!port)
+| _  -> failwith "Too many arguments in the command"
